eb0714cafa24e1d701744ba0ea3b6c3b9875c398
Kimsunfang <kimsunfang@gmail.com>
2022-11-13 16:23:44 +0900

    chore : Set the project

---
+++ src/test/java/com/encore/petandbe/PetandbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.encore.petandbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetandbeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

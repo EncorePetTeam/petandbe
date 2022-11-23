c6a75b83f09617a89278a5bfe84aef66ce559dc0
HunkiKim <gnsrl76@naver.com>
2022-11-23 13:15:55 +0900

    #39 Create Room CRUD Controller Test

---
+++ src/main/java/com/encore/petandbe/controller/accommodation/room/requests/RoomRegistrationRequest.java
@@ -0,0 +1,26 @@
+package com.encore.petandbe.controller.accommodation.room.requests;
+
+import com.encore.petandbe.model.accommodation.filtering.category.PetCategory;
+
+import lombok.Getter;
+
+@Getter
+public class RoomRegistrationRequest {
+
+	private Long accommodationId;
+	private String roomName;
+	private Integer amount;
+	private PetCategory petCategory;
+	private String weight;
+	private String detailInfo;
+
+	public RoomRegistrationRequest(Long accommodationId, String roomName, Integer amount,
+		PetCategory petCategory, String weight, String detailInfo) {
+		this.accommodationId = accommodationId;
+		this.roomName = roomName;
+		this.amount = amount;
+		this.petCategory = petCategory;
+		this.weight = weight;
+		this.detailInfo = detailInfo;
+	}
+}

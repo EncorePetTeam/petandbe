4d23528772917357618cccc857f582a6efd54e9c
HyungJoo Jang <hyungjoo1470@gmail.com>
2022-12-12 17:19:03 +0900

    #82 Setup docker-compose structure

--- app/src/main/java/com/encore/petandbe/interceptor/PermissionInterceptor.java
+++ app/src/main/java/com/encore/petandbe/interceptor/PermissionInterceptor.java
@@ -35,7 +35,6 @@ public class PermissionInterceptor implements HandlerInterceptor {
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-		System.out.println("----- 호출 -----");
 		if (checkHandlerMethod(handler) && !checkPermission(handler)) {
 			return true;
 		}

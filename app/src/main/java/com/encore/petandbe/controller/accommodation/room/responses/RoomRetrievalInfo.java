12a004571ad891e2fef726994f4c759d9ce5310f
Kimsunfang <kimsunfang@gmail.com>
2022-12-24 23:06:34 +0900

    #104 Room Image File

--- app/src/main/java/com/encore/petandbe/controller/accommodation/room/responses/RoomRetrievalInfo.java
+++ app/src/main/java/com/encore/petandbe/controller/accommodation/room/responses/RoomRetrievalInfo.java
@@ -4,6 +4,8 @@ import com.encore.petandbe.model.accommodation.filtering.category.PetCategory;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 @Builder
 public class RoomRetrievalInfo {
@@ -14,13 +16,15 @@ public class RoomRetrievalInfo {
     private PetCategory petCategory;
     private String weight;
     private String detailInfo;
+    private List<String> imageFileUrlList;
 
-    public RoomRetrievalInfo(Long roomId, String roomName, Integer amount, PetCategory petCategory, String weight, String detailInfo) {
+    public RoomRetrievalInfo(Long roomId, String roomName, Integer amount, PetCategory petCategory, String weight, String detailInfo, List<String> imageFileUrlList) {
         this.roomId = roomId;
         this.roomName = roomName;
         this.amount = amount;
         this.petCategory = petCategory;
         this.weight = weight;
         this.detailInfo = detailInfo;
+        this.imageFileUrlList = imageFileUrlList;
     }
 }
--- app/src/main/java/com/encore/petandbe/controller/accommodation/room/responses/RoomRetrievalResponse.java
+++ app/src/main/java/com/encore/petandbe/controller/accommodation/room/responses/RoomRetrievalResponse.java
@@ -5,6 +5,8 @@ import com.encore.petandbe.model.accommodation.filtering.category.PetCategory;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 @Builder
 public class RoomRetrievalResponse {
@@ -15,14 +17,15 @@ public class RoomRetrievalResponse {
 	private PetCategory petCategory;
 	private String weight;
 	private String detailInfo;
+	private List<String> imageFileUrlList;
 
-	public RoomRetrievalResponse(Long accommodationId, String roomName, Integer amount,
-		PetCategory petCategory, String weight, String detailInfo) {
+	public RoomRetrievalResponse(Long accommodationId, String roomName, Integer amount, PetCategory petCategory, String weight, String detailInfo, List<String> imageFileUrlList) {
 		this.accommodationId = accommodationId;
 		this.roomName = roomName;
 		this.amount = amount;
 		this.petCategory = petCategory;
 		this.weight = weight;
 		this.detailInfo = detailInfo;
+		this.imageFileUrlList = imageFileUrlList;
 	}
 }
